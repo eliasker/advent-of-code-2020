8331c447c1792ffcae42d1975a459d96ef694931
Elias <eliasker@gmail.com>
2020-12-14 21:07:40 +0200

    Day 12 part 2

--- day12/day12.ts
+++ day12/day12.ts
@@ -8,7 +8,90 @@ enum Direction {
   W = 270,
 }
 
-class Ship {
+class Waypoint {
+  north: number;
+  east: number;
+  south: number;
+  west: number;
+
+  constructor(north: number, east: number, south: number, west: number) {
+    this.north = north;
+    this.east = east;
+    this.south = south;
+    this.west = west;
+  }
+
+  public moveWaypoint(dir: string, value: number): void {
+    if (dir === "N") this.north += value;
+    else if (dir === "E") this.east += value;
+    else if (dir === "S") this.south += value;
+    else if (dir === "W") this.west += value;
+  }
+
+  public rotateWaypoint(dir: string, value: number): void {
+    let rotate = 0;
+    if (dir === "R") {
+      rotate += value;
+    } else if (dir === "L") {
+      rotate += 360 - value;
+    }
+    const prevValues = this.getValues();
+    if (rotate === 90) {
+      this.north = prevValues.west;
+      this.east = prevValues.north;
+      this.south = prevValues.east;
+      this.west = prevValues.south;
+    } else if (rotate === 180) {
+      this.north = prevValues.south;
+      this.east = prevValues.west;
+      this.south = prevValues.north;
+      this.west = prevValues.east;
+    } else if (rotate === 270) {
+      this.north = prevValues.east;
+      this.east = prevValues.south;
+      this.south = prevValues.west;
+      this.west = prevValues.north;
+    }
+  }
+  public getValues() {
+    return {
+      north: this.north,
+      east: this.east,
+      south: this.south,
+      west: this.west,
+    };
+  }
+}
+
+class WaypointShip {
+  target = new Waypoint(1, 10, 0, 0);
+  north: number = 0;
+  south: number = 0;
+  east: number = 0;
+  west: number = 0;
+
+  public executeInstruction(instr: string): void {
+    const dir = instr.slice(0, 1);
+    const value = +instr.slice(1);
+    if (dir === "F") {
+      this.move(value);
+    } else if (dir === "R" || dir === "L") {
+      this.target.rotateWaypoint(dir, value);
+    } else if (dir === "N" || dir === "E" || dir === "S" || dir === "W") {
+      this.target.moveWaypoint(dir, value);
+    }
+  }
+
+  public move(times: number): void {
+    const waypointValues = this.target.getValues();
+    this.north += waypointValues.north * times;
+    this.east += waypointValues.east * times;
+    this.south += waypointValues.south * times;
+    this.west += waypointValues.west * times;
+  }
+}
+
+class SimpleShip {
   // ship starts facing east
   direction: number = 90; // N=0, S=180, E=90, W=270
   north: number = 0;
@@ -65,13 +148,22 @@ const manhattanDistance = (
 };
 
 const part1 = (lines: string[]) => {
-  const ship = new Ship();
+  const ship = new SimpleShip();
   lines.forEach((line) => ship.executeInstruction(line));
   return manhattanDistance(ship.north, ship.south, ship.east, ship.west);
 };
 
+const part2 = (lines: string[]) => {
+  const wpShip = new WaypointShip();
+  lines.forEach((line) => wpShip.executeInstruction(line));
+  return manhattanDistance(
+    wpShip.north,
+    wpShip.south,
+    wpShip.east,
+    wpShip.west
+  );
+};
+
 console.log("--- Day 12 ---");
-console.log(`Part 1: ${part1(input)}`);
-/*
-console.log(`Part 2:`);
-*/
+console.log(`Part 1: Using input as directions the sailed ${part1(input)} units (Manhattan distance).`);
+console.log(`Part 2: Navigating with waypoints ship sailed ${part2(input)} units (Manhattan distance).`); 
