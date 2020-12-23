092dab0b37b29c5a9c8e63ef59dcf23dcc538fbe
Elias <eliasker@gmail.com>
2020-12-23 13:54:53 +0200

    Day 10 part 2

--- day10/README.md
+++ day10/README.md
@@ -80,3 +80,33 @@ Here is a larger example:
 In this larger example, in a chain that uses all of the adapters, there are 22 differences of 1 jolt and 10 differences of 3 jolts.
 
 Find a chain that uses all of your adapters to connect the charging outlet to your device's built-in adapter and count the joltage differences between the charging outlet, the adapters, and your device. What is the number of 1-jolt differences multiplied by the number of 3-jolt differences?
+
+--- Part Two ---
+
+To completely determine whether you have enough adapters, you'll need to figure out how many different ways they can be arranged. Every arrangement needs to connect the charging outlet to your device. The previous rules about when adapters can successfully connect still apply.
+
+The first example above (the one that starts with 16, 10, 15) supports the following arrangements:
+```
+(0), 1, 4, 5, 6, 7, 10, 11, 12, 15, 16, 19, (22)
+(0), 1, 4, 5, 6, 7, 10, 12, 15, 16, 19, (22)
+(0), 1, 4, 5, 7, 10, 11, 12, 15, 16, 19, (22)
+(0), 1, 4, 5, 7, 10, 12, 15, 16, 19, (22)
+(0), 1, 4, 6, 7, 10, 11, 12, 15, 16, 19, (22)
+(0), 1, 4, 6, 7, 10, 12, 15, 16, 19, (22)
+(0), 1, 4, 7, 10, 11, 12, 15, 16, 19, (22)
+(0), 1, 4, 7, 10, 12, 15, 16, 19, (22)
+```
+(The charging outlet and your device's built-in adapter are shown in parentheses.) Given the adapters from the first example, the total number of arrangements that connect the charging outlet to your device is 8.
+
+The second example supports 19208 distinct arrangements. 
+
+You glance back down at your bag and try to remember why you brought so many adapters; there must be more than a trillion valid ways to arrange them! Surely, there must be an efficient way to count the arrangements.
+
+What is the total number of distinct ways you can arrange the adapters to connect the charging outlet to your device?
+
+
+Run solution:
+```
+advent-of-code-2020> npm run ts-node "./day10/day10.ts"
+advent-of-code-2020> npm run solve --day=10
+```
--- day10/day10.ts
+++ day10/day10.ts
@@ -1,22 +1,30 @@
 import { readNumbers } from "../util/files";
 
-// adapters rated for specific output joltage
-// output can be 1-3 lower than adapters rating
-// your device is 3 jolts higher than highest adapter
+// adapters are rated for specific output "joltage"
+// previous output can be 1-3 lower than following rating
+// rating of your device is 3 jolts higher than highest adapter
 // treat the charging outlet as 0
-
 const test = readNumbers("./day10/test.txt");
 const input = readNumbers("./day10/day10_input.txt");
 const sortedTest = test.sort((a, b) => a - b);
 const sortedInput = input.sort((a, b) => a - b);
 
-// charging outlet has rating of 0
-// your devices built in rating is adapter with biggest rating + 3
-const findDifferences = (sortedAdapters: number[]): number[] => {
-  let prev = sortedAdapters[0];
-  let diff1 = 1; // charging outlet
-  let diff3 = 1; // your device
-  sortedAdapters.forEach((adapter, index) => {
+// forming chain is easier if adapters are sorted
+// charging outlet (rating: 0) is inserted at the beginning
+// devices (rating: largest + 3) is added to the end
+const initialize = (numbers: number[]): number[] => {
+  let sorted = numbers.sort((a, b) => a - b);
+  sorted.unshift(0);
+  const largest = sorted[sorted.length - 1];
+  sorted.push(largest + 3);
+  return sorted;
+};
+
+const findDifferences = (adapters: number[]): number[] => {
+  let prev = adapters[0];
+  let diff1 = 0;
+  let diff3 = 0;
+  adapters.forEach((adapter) => {
     if (adapter - prev === 1) {
       diff1++;
     } else if (adapter - prev === 3) {
@@ -31,6 +39,34 @@ const multiplyDifferences = (diff: number[]): number => {
   return diff[0] * diff[1];
 };
 
+let memo = {};
+const findCombinations = (adapters: number[]) => {
+  const key = adapters.join(",");
+  if (key in memo) {
+    return memo[key];
+  }
+  let result = 1;
+  for (let i = 1; i < adapters.length - 1; i++) {
+    if (adapters[i + 1] - adapters[i - 1] <= 3) {
+      const newAdapters = [adapters[i - 1]].concat(adapters.slice(i + 1));
+      result += findCombinations(newAdapters);
+    }
+  }
+  memo[key] = result;
+  return result;
+};
+
+const initialized = initialize(input);
+
 console.log("--- Day 10 ---");
-console.log(`Part 1: ${multiplyDifferences(findDifferences(input))}`);
-console.log(`Part 2:`);
+console.log(
+  `Part 1: Number of 1&3 differences in adapters multiplied is: ${multiplyDifferences(
+    findDifferences(initialized)
+  )}`
+);
+
+console.log(
+  `Part 2: Number of distinct adapter arrangements: ${findCombinations(
+    initialized
+  )}`
+);
